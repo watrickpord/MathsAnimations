33e9d4eec709b7f694551db480cd1c96420bfe31
Patrick <58635201+watrickpord@users.noreply.github.com>
2021-01-25 16:48:36 +0000

    Added code to plot 3rd order polynomial also

--- ChebyshevAnimated.m
+++ ChebyshevAnimated.m
@@ -1,18 +1,19 @@
+%% setup to plot
 clear
-
 filename = 'Chebyshev.gif'
 
 x = linspace(-1, 1, 50);
 theta = linspace(0, 2*pi, 50);
 cos1 = cos(theta);
 cos2 = cos(2*theta);
+cos3 = cos(3*theta);
 
 % chebychev polynomials
 T2 = 2.*x.^2 - 1;
 T3 = 4.*x.^3 - 3.*x;
 
 
-%% plot animation
+%% plot T_2
 fig = figure
 fig.Color = 'white'
 
@@ -45,6 +46,71 @@ for i = 1:length(theta)
     plot(theta, cos2)
     plot(currentTheta, cos2(i), 'kx')
     lOut = line([0, currentTheta], [cos2(i), cos2(i)]);
+    lOut.Color = [0.1 0.1 0.1]; lOut.LineStyle = '--';
+    
+    
+    % input curve (cos x)
+    nexttile
+    hold on
+    xlabel('cos(\theta)')
+    ylabel('\theta')
+    yticks([0 pi/2 pi 3*pi/2 2*pi])     % label y axis with pi multiples
+    yticklabels({'0','\pi/2','\pi','3\pi/2','2\pi'})
+    plot(cos1, theta)
+    plot(cos1(i), currentTheta, 'kx')
+    lIn = line([cos1(i), cos1(i)], [currentTheta, 2*pi]);
+    lIn.Color = [0.1 0.1 0.1]; lIn.LineStyle = '--';
+    
+    % force draw of frame
+    drawnow
+    
+    % Capture the plot as an image
+    frame = getframe(fig);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    
+    % Write to the GIF File
+    if i == 1
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append');
+    end
+end
+
+
+%% plot T_3
+fig = figure
+fig.Color = 'white'
+
+for i = 1:length(theta)
+    currentTheta = theta(i);
+    
+    % draw frame data to plot
+    clf
+    t = tiledlayout(2,2);
+    title(t,'Chebyshev Polynomial T_3 = 4x^3 - 3x')
+    
+    % transfer function (i.e. chebyshev polynomial)
+    nexttile
+    hold on
+    xlabel('x')
+    ylabel('T_3(x)')
+    plot(x, T3, 'r')     % plot chebychev curve
+    lVert = line([cos1(i),cos1(i)], [-1,cos3(i)]);   % vertical (input) line
+    lHorz = line([cos1(i),1], [cos3(i),cos3(i)]);    % horizontal (output) line
+    lVert.Color = [0.1 0.1 0.1]; lHorz.Color = [0.1 0.1 0.1];
+    lVert.LineStyle = '--'; lHorz.LineStyle = '--';
+    
+    % output curve (cos nx)
+    nexttile
+    hold on
+    ylabel('cos(3\theta)')
+    xlabel('\theta')
+    xticks([0 pi/3 2*pi/3 pi 4*pi/3 5*pi/3 2*pi])     % label x axis with pi multiples
+    xticklabels({'0','\pi/3','2\pi/3','\pi','4\pi/3','5\pi/3','2\pi'})
+    plot(theta, cos3)
+    plot(currentTheta, cos3(i), 'kx')
+    lOut = line([0, currentTheta], [cos3(i), cos3(i)]);
     lOut.Color = [0.1 0.1 0.1]; lOut.LineStyle = '--';
     
     
