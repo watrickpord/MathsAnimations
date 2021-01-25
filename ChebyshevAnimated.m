c604fcc02d63abd39ab7f0d9b0d2fc8ff0fd6c76
Patrick <58635201+watrickpord@users.noreply.github.com>
2021-01-25 16:41:32 +0000

    Added code for outputting gif

--- ChebyshevAnimated.m
+++ ChebyshevAnimated.m
@@ -1,3 +1,6 @@
+clear
+
+filename = 'Chebyshev.gif'
 
 x = linspace(-1, 1, 50);
 theta = linspace(0, 2*pi, 50);
@@ -8,16 +11,20 @@ cos2 = cos(2*theta);
 T2 = 2.*x.^2 - 1;
 T3 = 4.*x.^3 - 3.*x;
 
-figure
 
-tiledlayout(2,2)
+%% plot animation
+fig = figure
+fig.Color = 'white'
 
-% plot animation
 for i = 1:length(theta)
     currentTheta = theta(i);
     
     % draw frame data to plot
     clf
+    t = tiledlayout(2,2);
+    title(t,'Chebyshev Polynomial T_2 = 2x^2 - 1')
+    
+    % transfer function (i.e. chebyshev polynomial)
     nexttile
     hold on
     xlabel('x')
@@ -28,19 +35,26 @@ for i = 1:length(theta)
     lVert.Color = [0.1 0.1 0.1]; lHorz.Color = [0.1 0.1 0.1];
     lVert.LineStyle = '--'; lHorz.LineStyle = '--';
     
+    % output curve (cos nx)
     nexttile
     hold on
     ylabel('cos(2\theta)')
     xlabel('\theta')
+    xticks([0 pi/2 pi 3*pi/2 2*pi])     % label x axis with pi multiples
+    xticklabels({'0','\pi/2','\pi','3\pi/2','2\pi'})
     plot(theta, cos2)
     plot(currentTheta, cos2(i), 'kx')
     lOut = line([0, currentTheta], [cos2(i), cos2(i)]);
     lOut.Color = [0.1 0.1 0.1]; lOut.LineStyle = '--';
     
+    
+    % input curve (cos x)
     nexttile
     hold on
     xlabel('cos(\theta)')
     ylabel('\theta')
+    yticks([0 pi/2 pi 3*pi/2 2*pi])     % label y axis with pi multiples
+    yticklabels({'0','\pi/2','\pi','3\pi/2','2\pi'})
     plot(cos1, theta)
     plot(cos1(i), currentTheta, 'kx')
     lIn = line([cos1(i), cos1(i)], [currentTheta, 2*pi]);
@@ -48,5 +62,16 @@ for i = 1:length(theta)
     
     % force draw of frame
     drawnow
+    
+    % Capture the plot as an image
+    frame = getframe(fig);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    
+    % Write to the GIF File
+    if i == 1
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append');
+    end
 end
-
